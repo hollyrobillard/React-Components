55119b820a5855b60f4840b5ff836d01f228a50a
Holly Ann Robillard <103912261+hollyrobillard@users.noreply.github.com>
2022-09-13 08:31:33 -0400

    Font Awesome added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+import { faHatCowboy } from '@fortawesome/pro-thin-svg-icons'
+import { faHatChef } from '@fortawesome/sharp-solid-svg-icons'
+
+library.add(fas, faTwitter, faFontAwesome, faHatCowboy, faHatChef)
+
+import React from "react"
+import Header from "./Header"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Header />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
