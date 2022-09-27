47bcb1f13498d3c9c66cae1516925884bc1b1aa0
Holly Ann Robillard <103912261+hollyrobillard@users.noreply.github.com>
2022-09-27 09:03:51 -0400

    Home Page Build

--- react-components/pages/HomeBody.js
+++ react-components/pages/HomeBody.js
@@ -4,8 +4,11 @@ export default function HomeBody() {
     return(
         <section className="HomeBody">
             <article className="welcome">
-                <h1>Welcome!</h1>
-                <h2>This site is your go-to for code for open source components</h2>
+                <h1>Welcome</h1>
+                <h2>This site is your go-to for open source component code</h2>
+                <p className="welcome-subtext text-start">tweak it</p>
+                <p className="welcome-subtext text-center">style it</p>
+                <p className="welcome-subtext text-end">make it yours</p>
             </article>
         </section>
     )
